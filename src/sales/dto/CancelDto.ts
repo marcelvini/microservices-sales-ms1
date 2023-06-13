9f4e6c04db61690313925b97152dbb30f6350b41
marcelvini <marcelviniccius@gmail.com>
2023-06-13 00:18:51 -0300

    add swagger doc

--- src/sales/dto/CancelDto.ts
+++ src/sales/dto/CancelDto.ts
@@ -1,6 +1,19 @@
-type CancelDto = {
+import { ApiProperty } from '@nestjs/swagger';
+class CancelDto {
+	@ApiProperty({
+		example: '318440fb7491',
+		description: 'its the service key.',
+	})
 	serviceKey: string;
+	@ApiProperty({
+		example: 'any@email.com',
+		description: 'its the buyer email.',
+	})
 	buyerEmail: string;
+	@ApiProperty({
+		example: '123456',
+		description: 'its the product id.',
+	})
 	productId: string;
-};
+}
 export { CancelDto };
